714ad79e115bac763cee31c893e829a730e43824
Kylie Paykoff <kyliepaykoff@gmail.com>
2024-08-03 20:55:35 -0400

    initial commit

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            BankAccount account = new BankAccount();
+
+            Console.WriteLine($"How much would you like to deposit?");
+            double amountDeposited = double.Parse(Console.ReadLine());
+
+
+            account.Deposit(amountDeposited);
+            double userBalance = account.GetBalance();
+            Console.WriteLine($"Your current balance is {userBalance, 0:c}");
+
+
+
+
+
         }
     }
 }
